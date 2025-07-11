558fc8ef81fde821a2c6a7830ee2522b7e8c36e6
Hunsa Semako S. <smkmayomisamuel@gmail.com>
2025-07-11 05:26:53 +0100

    Updated

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./contexts/AuthContext";
 import "./index.css";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
